ab8c95404866e0b2cf5c11b917a39268e2034588
Alex van Vliet <alex@vanvliet.pro>
2019-04-21 22:42:43 +0200

    tests(lexer): add begin token

--- tests/parser/lexer.cc
+++ tests/parser/lexer.cc
@@ -7,5 +7,13 @@ SCENARIO("The lexer can parse tokens", "[lexer]") {
     GIVEN("A lexer with a stream") {
         auto stream = std::stringstream();
         auto lexer = parser::lexer(stream);
+        WHEN("no token was eaten") {
+            THEN("the token is begin") {
+                REQUIRE(lexer.current() == parser::token::BEGIN);
+            }
+            THEN ("the value is null") {
+                REQUIRE(lexer.value() == '\0');
+            }
+        }
     }
 }
