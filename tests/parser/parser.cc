d12cdb6e5469c353becba80e6413a1dcb8e3c5f0
Alex van Vliet <alex@vanvliet.pro>
2019-04-22 11:59:48 +0200

    tests(parser): add character node

--- tests/parser/parser.cc
+++ tests/parser/parser.cc
@@ -6,6 +6,16 @@ SCENARIO("The parser can build an AST", "[parser]") {
     GIVEN("A parser with a lexer") {
         auto stream = std::stringstream();
         auto lexer = parser::lexer(stream);
-        auto parser : parser::parser(lexer);
+        auto parser = parser::parser(lexer);
+        WHEN("The next token is a character") {
+            stream << "t";
+            THEN("The AST contains only a token node") {
+                parser::ast::node* node = parser.parse();
+                parser::ast::character* ast_token = dynamic_cast<parser::ast::character*>(node);
+                THEN("The node contains the character") {
+                    REQUIRE(ast_token.value() == 't');
+                }
+            }
+        }
     }
 }
