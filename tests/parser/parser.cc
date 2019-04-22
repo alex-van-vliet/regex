dbd9c4f8256d04e75f2ff3cd45fb7a2f6f0161e9
Alex van Vliet <alex@vanvliet.pro>
2019-04-22 15:30:08 +0200

    typo(tests): lowercase whens and thens

--- tests/parser/parser.cc
+++ tests/parser/parser.cc
@@ -11,16 +11,16 @@ SCENARIO("The parser can build an AST", "[parser]") {
         auto lexer = ast::parser::lexer(input);
         auto parser = ast::parser::parser(lexer);
         auto printer = ast::visitor::prettyprinter(output);
-        WHEN("The next token is a character") {
+        WHEN("the next token is a character") {
             input << "t";
-            THEN("The AST contains only a token node") {
+            THEN("the AST contains only a token node") {
                 ast::node* node = parser.parse();
                 ast::character* ast_token = dynamic_cast<ast::character*>(node);
-                THEN("The node contains the character") {
+                THEN("the node contains the character") {
                     REQUIRE(ast_token->value() == 't');
                     delete(node);
                 }
-                THEN("The pretty printed AST is the same") {
+                THEN("the pretty printed AST is the same") {
                     printer(node);
                     REQUIRE(output.str() == "t");
                     delete(node);
