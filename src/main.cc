d727907f2d5eab70223533d12a08cfcfa32e226a
Alex van Vliet <alex@vanvliet.pro>
2019-04-22 15:22:05 +0200

    feat: add basic main with only the pretty printer

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "lexer.hh"
+#include "parser.hh"
+#include "prettyprinter.hh"
 
 int main()
 {
+    auto lexer = ast::parser::lexer(std::cin);
+    auto parser = ast::parser::parser(lexer);
+    auto* ast = parser.parse();
+    auto printer = ast::visitor::prettyprinter(std::cout);
+    ast->accept(printer);
+    delete ast;
     return 0;
 }
