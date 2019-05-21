d4ac198b25bf1e9f5c16fbfd168e1228b63d1ea7
Alex van Vliet <alex@vanvliet.pro>
2019-05-21 16:27:34 +0200

    tests(graph): create a state

--- tests/automaton/graph.cc
+++ tests/automaton/graph.cc
@@ -6,5 +6,22 @@ SCENARIO("The graph can link states with transitions", "[graph]")
 {
     GIVEN("A graph") {
         automaton::graph graph;
+        WHEN("A state is added to the graph")
+        {
+            automaton::state* state = graph.new_state();
+            THEN("The state is not null")
+            {
+                REQUIRE(state != nullptr);
+            }
+
+            THEN("The graph contains the state")
+            {
+                bool found = false;
+                for (const auto& other : graph.get_states())
+                    if (other.get() == state)
+                        found = true;
+                REQUIRE(found);
+            }
+        }
     }
 }
