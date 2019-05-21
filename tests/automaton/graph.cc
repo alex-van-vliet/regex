10196ea2aa33fe2cd69ef57052489279f63f9808
Alex van Vliet <alex@vanvliet.pro>
2019-05-21 18:12:13 +0200

    tests(graph): add an epsilon transition

--- tests/automaton/graph.cc
+++ tests/automaton/graph.cc
@@ -1,11 +1,59 @@
 #include <sstream>
 #include <catch.hpp>
+#include <algorithm>
 #include "automaton/graph.hh"
+#include "automaton/transition.hh"
+#include "automaton/transitions/epsilon.hh"
 
 SCENARIO("The graph can link states with transitions", "[graph]")
 {
-    GIVEN("A graph") {
+    GIVEN("A graph with two states") {
         automaton::graph graph;
+        automaton::state* state1 = graph.new_state();
+        automaton::state* state2 = graph.new_state();
+
+        WHEN("An epsilon transition from state1 to state2 is added to the graph")
+        {
+            automaton::transition* transition = graph.new_transition<automaton::transitions::epsilon>(state1, state2);
+
+            THEN("The transition is an epsilon transition")
+            {
+                automaton::transitions::epsilon* epsilon = dynamic_cast<automaton::transitions::epsilon*>(transition);
+                REQUIRE(epsilon != nullptr);
+            }
+
+            THEN("The source of the transition is state1")
+            {
+                REQUIRE(transition->get_source() == state1);
+            }
+
+            THEN("The destination of the transition is state2")
+            {
+                REQUIRE(transition->get_destination() == state2);
+            }
+
+            THEN("The transition is in the out set of state1")
+            {
+                REQUIRE(std::find(state1->get_out_transitions().begin(),
+                    state1->get_out_transitions().end(), transition) != state1->get_out_transitions().end());
+            }
+
+            THEN("The transition is in the in set of state2")
+            {
+                REQUIRE(std::find(state2->get_in_transitions().begin(),
+                    state2->get_in_transitions().end(), transition) != state2->get_in_transitions().end());
+            }
+
+            THEN("The graph contains the transition")
+            {
+                bool found = false;
+                for (const auto& other : graph.get_transitions())
+                    if (other.get() == transition)
+                        found = true;
+                REQUIRE(found);
+            }
+        }
+
         WHEN("A state is added to the graph")
         {
             automaton::state* state = graph.new_state();
