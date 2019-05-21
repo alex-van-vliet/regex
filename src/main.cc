c907e465b7ccc38af598d7fbad4cf58f3d6477d7
Alex van Vliet <alex@vanvliet.pro>
2019-05-21 14:25:54 +0200

    feat(main): show the alphabet

--- src/main.cc
+++ src/main.cc
@@ -2,14 +2,24 @@
 #include "lexer.hh"
 #include "parser.hh"
 #include "prettyprinter.hh"
+#include "alphabet_finder.hh"
 
 int main()
 {
     auto lexer = ast::parser::lexer(std::cin);
     auto parser = ast::parser::parser(lexer);
     auto* ast = parser.parse();
+
     auto printer = ast::visitor::prettyprinter(std::cout);
-    ast->accept(printer);
+    printer(ast);
+
+    auto alphabet_finder = ast::visitor::alphabet_finder();
+    alphabet_finder(ast);
+
+    std::cout << std::endl << "Letters: ";
+    for (char it : alphabet_finder.letters())
+        std::cout << it;
+    std::cout << std::endl;
     delete ast;
     return 0;
 }
